1c7494c78f6cc524b9025c6463295731e8779243
jacksonsix <jacksonsixmailbox@gmail.com>
2018-03-07 13:35:14 -0500

    Update compiler.js

--- compiler.js
+++ compiler.js
@@ -50,54 +50,6 @@ function getType(exp){
 	return exp.type;
 }
 
-// define the data structure of instruction sequenece
-function make_instruction_sequence(reg_need,reg_modefiy,instructions){
-	var s = {};
-	s.reg_need     = reg_need;
-	s.reg_modefiy = reg_modefiy;
-	s.instructions  = instructions;
-	return s;
-}
-
-
-function append_instruction_sequence(seq1,seq2){
-	return seq1 + seq2;
-}
-
-function parallel_instruction_sequence(seq1,seq2){
-	
-}
-//preserving procedure
-// when registers need to be saved, 
-
-function preserving(reg_set,seq1,seq2){
-	var saves =[];
-	var restores = [];
-	for(var i=0;i< reg_set.length;i++){
-		var reg = reg_set[i];
-		var save_inst = '(save '+ reg +')';
-		var restore_inst = '(restore ' + reg + ')';
-		saves.push(save_inst);
-		restores.unshift(restore_inst);
-	}
-
-	return append_instruction_sequence(
-	              append_instruction_sequence(
-	                  append_instruction_sequence(saves,seq1)
-					  ,restores)
-		          ,seq2);	  
-}
-
-
-// compile_linkage , how code do the next instruction
-function empty_instruction_sequence(){
-	var s ={
-			s.reg_need    = [],
-	       s.reg_modefiy = [],
-	       s.instructions  = []
-	};
-	return s;
-}
 
 function compile_linkage(linkage){
 	var result ;
@@ -219,14 +171,14 @@ function compile_lambda(exp,target,linkage){
 	var label_counter = new_label_counter()();
 	var proc_entry = make_label('entry'+label_counter);
 	var after_lambda = make_label('after_lambda'+label_counter);
-	var lambada_linkage = linkage==='next'? after_lambda: linkage;
+	var lambda_linkage = linkage==='next'? after_lambda: linkage;
 	
 	return append_instruction_sequence(
 	              tack_on_instruction_sequence(
-		                end_with_linkage(lambada_linkage
+		                end_with_linkage(lambda_linkage
 										  ,make_instruction_sequence(['env']
 																						,[target]
-																						,['(assign target (op make_compiled_proc) (label proc_entry) (reg env))']))
+																						,['(assign target (op make_compiled_proc) (label '+proc_entry'+) (reg env))']))
 		                ,(compile_lambda_body( exp, proc_entry)))
 			      ,after_lambda);  
 }
@@ -241,5 +193,237 @@ function compile_lambda_body(exp, proc_entry){
 	                                                ,compile_sequence(lambda_body(exp), 'val','return'));
 	
 }
+
+function compile_application(exp,target,linkage){
+	var proc_code = compile( app_operator(exp), 'proc','next');
+	var oprands_code = app_oprands(exp).map(function(oprand){
+		compile(oprand, 'val','next');
+	});
+	
+	return preserving( ['env','continue']
+	                  ,proc_code
+					  ,preserving(['proc','continue']
+					                      ,construct_arglist(oprands_code)
+										  ,compile_proc_call(target,linkage)));
+} 
+
+function construct_arglist( oprands_code){		
+	if(oprands_code==null ||  oprands_code.length ==0){
+		make_instruction_sequence(['']
+													 ,['argl']
+													 ,['(assign argl (const ()))']);
+	}else{
+		var oprands_code = oprands_code.reverse();
+		var code_to_get_last_arg  = append_instruction_sequence( oprands_code[0]
+		                                                                                             ,make_instruction_sequence(['val']
+																									                                               ,['argl']
+																																				   ,['(assign argl (op list) (reg val))']));
+		var rest_oprands  = oprands_code.slice(1);
+		if(rest_oprands.length ==0){
+			return code_to_get_last_arg;
+		}else{
+			return preserving( ['env']
+			                             ,code_to_get_last_arg
+										 ,code_to_get_rest_args(rest_oprands));
+		}																																		   
+	}
+}
+
+function code_to_get_rest_args(oprands_code){
+	var code_for_next_arg = preserving(['argl']
+	                                                       ,oprands_code[0]
+														   ,make_instruction_sequence(['val','argl']
+														                                                ,['argl']
+																										,['(assign argl (op cons) (reg val) (reg argl))']));
+	var rest_oprands = oprands_code.slice(1);
+    if(rest_oprands.length ==0){
+		return code_for_next_arg
+	}else{
+		return preserving(['env']
+		                           ,code_for_next_arg
+								   ,code_to_get_rest_args(rest_oprands));
+	}	
+}
+
+
+function compile_proc_call(target,linkage){
+	var prim_branch = make_label('prim_branch');
+	var compiled_branch = make_label('compiled_branch');
+	var after_call = make_label('after_call');
+	//
+	var compiled_linkage = linkage ==='next'? after_call: linkage;
+	return append_instruction_sequence(make_instruction_sequence(['proc']
+	                                                                                            ,['']
+																								,['(test (op primitive)  (reg proc))'
+																								  ,'(branch (label '+prim_branch+'))'])
+	                                               ,parallel_instruction_sequence(append_instruction_sequence(compiled_branch
+												                                                                                                    ,compile_proc_appl(target,compiled_linkage))
+																									,append_instruction_sequence(prim_branch
+																									,end_with_linkage(linkage,make_instruction_sequence(['proc','argl']
+																									                                                                                     ,[target]
+																																														 ,['(assign target (op apply_prim_procedure) (reg proc) (reg argl))']))))
+													,after_call);
+}
+
+
+function compile_proc_appl(target,linkage){
+	if( target==='val' && linkage !=='return'){
+		return make_instruction_sequence( ['proc']
+		                                                      ,['val','proc','argl','continue','exp']
+															  ,['(assign continue (label '+linkage+'))'
+															     ,'(assign val (op compiled_procedure_entry) (reg proc))'
+																 ,'(goto (reg val))']);
+	}else if(target !=='val' && linkage !== 'return'){
+		var proc_return = make_label('proc_return');
+		return make_instruction_sequence( ['proc']
+		                                                        ,['val','proc','argl','continue','exp']
+																,['(assign continue (label '+proc_return+'))'
+																  ,'(assign val (op compiled_procedure_entry) (reg proc))'
+																  ,'(goto (reg val))'
+																  ,proc_return
+																  ,'(assign '+target+' (reg val))'
+																  ,'(goto (label '+linkage+'))']);
+	}else if(target ==='val' && linkage ==='return'){
+		return make_instruction_sequence(['proc','continue']
+		                                                       ,['val','proc','argl','continue','exp']
+															   ,['(assign val (op compiled_procedure_entry) (reg proc))'
+															   ,'(goto (reg val))']);
+	}else if(target !=='val' && linkage ==='return'){
+		console.log('error linkage,target ');
+	}else{
+		console.log('cannot reach here ');
+	}
+}
+
+
+// details of how instruction sequences are combined.
+function registers_needed(s){
+	return s.needed;
+}
+
+function registers_modified(s){
+	return s.modified;
+}
+
+function registers_statements(s){
+	return s.statement;
+}
+
+function needs_registers(sequence,reg){
+	return -1 !== registers_needed(sequence).indexOf(reg);
+}
+
+function modifies_registers(sequence,reg){
+	return -1 !== registers_modified(sequence).indexOf(reg);
+}
+
+function append_instruction_sequence(seq1,seq2){
+	function append_2_seq(seq1,seq2){
+		make_instruction_sequence(set_union(registers_needed(seq1), set_dif(registers_needed(seq2), registers_modified(seq1)))
+			                                        ,set_union(registers_modified(seq1), registers_modified(seq2))
+												    , registers_statements(seq1).append(registers_statements(seq2)));
+													
+	}
+	append_2_seq(seq1,seq2);
+}
+
+
+//preserving procedure
+// when registers need to be saved, 
+
+function check(reg,seq1,seq2){
+	if(needs_registers(seq2, reg) && modifies_registers(seq1,reg)){
+		return true;
+	}
+	return false;
+}
+
+function preserving(reg_set,seq1,seq2){
+	var saves =[];
+	var restores = [];
+	for(var i=0;i< reg_set.length;i++){
+		var reg = reg_set[i];
+		if(check(reg,seq1,seq2)){
+		   var save_inst = '(save '+ reg +')';
+		   var restore_inst = '(restore ' + reg + ')';
+		   saves.push(save_inst);
+		   restores.unshift(restore_inst);
+		}
+	}
+
+	return append_instruction_sequence(
+	              append_instruction_sequence(
+	                  append_instruction_sequence(saves,seq1)
+					  ,restores)
+		          ,seq2);	  
+}
+
+// define the data structure of instruction sequenece
+function make_instruction_sequence(reg_need,reg_modefiy,instructions){
+	var s = {};
+	s.reg_need     = reg_need;
+	s.reg_modefiy = reg_modefiy;
+	s.instructions  = instructions;
+	return s;
+}
+
+// basic operation of make data structure. ->  append  <- 
+function parallel_instruction_sequence(seq1,seq2){
+	
+}
+
+// compile_linkage , how code do the next instruction
+function empty_instruction_sequence(){
+	var s ={
+			s.reg_need    = [],
+	       s.reg_modefiy = [],
+	       s.instructions  = []
+	};
+	return s;
+}
+
+function set_union(s1,s2){
+	if(s1 == null || s1.length ==0){
+		return s2;
+	}else{
+		var first = s1[0];
+		if(s2.indexOf(first) ==-1){
+			return set_union(s1.slice(1),s2).unshift(first);
+		}else{
+			return set_union(s1.slice(1),s2);
+		}
+		
+	}
+}
+
+function set_dif(s1,s2){
+	if(s1 ==null || s1.length ==0){
+		return [];
+	}else{
+		var first = s1[0];
+		if(s2.indexOf(first) ==-1){
+			return set_union(s1.slice(1),s2).unshift(first);
+		}else{
+			return set_union(s1.slice(1),s2);
+		}
+		
+	}
+}
+
+
+function tack_on_instruction_sequence(seq, body_seq){
+	return make_instruction_sequence( registers_needed(seq)
+	                                             , registers_modified(seq)
+												 , registers_statements(seq).append(registers_statements(body_seq)));
+}
+
+function parallel_instruction_sequence(seq1,seq2){
+	return make_instruction_sequence(set_union(registers_needed(seq1), registers_needed(seq2))
+	                                                      ,set_union(registers_modified(seq1),registers_modified(seq2))
+														  ,registers_statements(seq1).append(registers_statements(seq2)));
+	                                                                      
+}
+
+
 
 
